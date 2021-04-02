02d836ae95ccb4c33d159d09a6529e4db109bab9
Idan Joshua Garay <idangaray@Idans-MacBook-Pro.local>
2021-04-02 19:25:16 +0800

    chart

--- src/components/Charts/Charts.jsx
+++ src/components/Charts/Charts.jsx
@@ -1,4 +1,25 @@
+import React, { useState, useEffect } from 'react';
+import { fetchDailyData } from '../../api';
+import { Line, Bar } from 'react-chartjs-2';
+
+import styles from  './Charts.module.css';
+
 const Charts = () => {
+    const {dailyData, setDailyData} = setState({});
+
+    useEffect(() => {
+        const fetchAPI = async () => {
+            setDailyData(await fetchDailyData());
+        }
+        fetchAPI();
+    });
+
+    const lineChart = (<Line data={{
+        labels: '',
+        dataSets: [{}, {}],
+    }}/>);
+    //50:02
+
     return (
         <div>
             <h1>Charts</h1>
