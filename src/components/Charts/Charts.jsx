e12eb677e9c637b86b30482d18fd39057151b426
Idan Joshua Garay <idangaray@Idans-MacBook-Pro.local>
2021-04-04 12:52:07 +0800

    chart

--- src/components/Charts/Charts.jsx
+++ src/components/Charts/Charts.jsx
@@ -2,27 +2,52 @@ import React, { useState, useEffect } from 'react';
 import { fetchDailyData } from '../../api';
 import { Line, Bar } from 'react-chartjs-2';
 
-import styles from  './Charts.module.css';
+import styles from './Charts.module.css';
 
 const Charts = () => {
-    const {dailyData, setDailyData} = setState({});
+    const [dailyData, setDailyData] = useState({});
 
     useEffect(() => {
         const fetchAPI = async () => {
             setDailyData(await fetchDailyData());
         }
         fetchAPI();
+
+        console.log(dailyData);
     });
 
-    const lineChart = (<Line data={{
-        labels: '',
-        dataSets: [{}, {}],
-    }}/>);
-    //50:02
+    const lineChart = (
+        dailyData[0] ? (
+          <Line
+            data={{
+              labels: dailyData.map(({ date }) => new Date(date).toLocaleDateString()),
+              datasets: [{
+                data: dailyData.map((data) => data.confirmed),
+                label: 'Infected',
+                borderColor: '#3333ff',
+                fill: true,
+              }, {
+                data: dailyData.map((data) => data.deaths),
+                label: 'Deaths',
+                borderColor: 'red',
+                backgroundColor: 'rgba(255, 0, 0, 0.5)',
+                fill: true,
+              },  {
+                data: dailyData.map((data) => data.recovered),
+                label: 'Recovered',
+                borderColor: 'green',
+                backgroundColor: 'rgba(0, 255, 0, 0.5)',
+                fill: true,
+              },
+              ],
+            }}
+          />
+        ) : null
+      );
 
     return (
-        <div>
-            <h1>Charts</h1>
+        <div className={styles.container}>
+            {lineChart}
         </div>
     )
 }
